ac027a694a03be44eed8314a00dc65bc60584e7c
Rodrigo Alvarenga <raalvarenga423@gmail.com>
2025-05-13 23:12:31 -0700

    Fix directory for generated prisma

--- prisma-post-script.sh
+++ prisma-post-script.sh
@@ -2,7 +2,7 @@
 
 echo "Fixing '__dirname' and '__filename' in Prisma generated files..."
 
-TARGET_DIR="./prisma/generated"
+TARGET_DIR="./generated/prisma"
 
 # find "$TARGET_DIR" -type f \( -name "*.js" -o -name "*.mjs" \) -exec sed -i \
 #   "s|__dirname|new URL('.', import.meta.url).pathname|g" {} +
