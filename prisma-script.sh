303b5358bbf7b6494b34f9db17042e766723d9c0
Rodrigo Alvarenga <raalvarenga423@gmail.com>
2025-05-13 22:59:02 -0700

    Fix prisma use of dirname

---
+++ prisma-script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Fixing '__dirname' and '__filename' in Prisma generated files..."
+
+TARGET_DIR="./prisma/generated"
+
+# find "$TARGET_DIR" -type f \( -name "*.js" -o -name "*.mjs" \) -exec sed -i \
+#   "s|__dirname|new URL('.', import.meta.url).pathname|g" {} +
+
+find "$TARGET_DIR" -type f \( -name "*.js" -o -name "*.mjs" \) \
+  -exec sed -i 's/__dirname/new URL(import.meta.url).pathname/g' {} +
+
+find "$TARGET_DIR" -type f \( -name "*.js" -o -name "*.mjs" \) \
+  -exec sed -i 's/__filename/new URL(import.meta.url).pathname/g' {} +
+
+echo "Prisma patching complete."
