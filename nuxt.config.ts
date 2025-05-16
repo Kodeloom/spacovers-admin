79c8616763ac788a239108642536e3f8ffc3fe7f
Rodrigo Alvarenga <raalvarenga423@gmail.com>
2025-05-16 01:45:15 -0700

    Add vite resolve alias

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,17 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   ssr: true,
   devtools: { enabled: process.env.NODE_ENV === 'development' },
+  vite: {
+  resolve: {
+    alias: {
+      'node:console': 'console',
+    }
+  }
+},
   nitro: {
   //   preset: 'node-server',
     externals: {
-      inline: ['@prisma/client', '@zenstackhq/runtime/edge'],
+      inline: ['@prisma/client', '@zenstackhq/runtime/edge', '@zenstackhq/tanstack-query'],
     },
     preset: "cloudflare_pages",
       cloudflare: {
